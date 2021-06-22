ec3c642d2799f6ec07480700c0daf29c572a317d
crincon <crincon@grupodot.com>
2021-06-21 23:55:16 -0500

    pruebas de autenticacion

--- bot/main.py
+++ bot/main.py
@@ -5,19 +5,25 @@ from settings import Config, logger
 CONFIG = Config
 
 
-auth = tweepy.OAuthHandler(CONFIG.API_KEY.value, CONFIG.API_SECRET_KEY.value)
+auth = tweepy.OAuthHandler(
+    CONFIG.API_KEY.value, CONFIG.API_SECRET_KEY.value)
 auth.set_access_token(CONFIG.ACCESS_TOKEN.value,
                       CONFIG.ACCESS_TOKEN_SECRET.value)
+api = tweepy.API(auth, wait_on_rate_limit=True,
+                 wait_on_rate_limit_notify=True)
 
-api = tweepy.API(auth)
 
-try:
-    api.verify_credentials()
-    logger.info('Authentication OK')
-except:
-    logger.error('Error during authentication')
+class TwitterBot():
+
+    def auth(self):
+        try:
+            api.verify_credentials()
+            logger.info('Authentication OK')
+        except:
+            logger.error('Error during authentication')
 
 
 if __name__ == '__main__':
 
-    pass
+    tw_bot = TwitterBot()
+    tw_bot.auth()
